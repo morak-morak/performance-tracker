f22ee905fa5910dd00f376c5610497b4ddb37acb
cjlee38 <cjlee38@github.com>
2022-12-18 17:15:09 +0900

    feat: 로깅 설정 적용

--- src/main/java/com/morak/performancetracker/junit/PerformanceTrackerSetupExtension.java
+++ src/main/java/com/morak/performancetracker/junit/PerformanceTrackerSetupExtension.java
@@ -1,16 +1,30 @@
 package com.morak.performancetracker.junit;
 
-import com.morak.performancetracker.aop.PerformanceMonitorAop;
+import com.morak.performancetracker.aop.persistence.PerformanceMonitor;
+import com.morak.performancetracker.aop.persistence.PerformanceMonitorAop;
+import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-public class PerformanceTrackerSetupExtension implements BeforeEachCallback {
+public class PerformanceTrackerSetupExtension implements BeforeEachCallback, AfterEachCallback {
+
+    private final Logger log = LoggerFactory.getLogger("PERFORMANCE");
+
     @Override
-    public void beforeEach(ExtensionContext context) throws Exception {
+    public void beforeEach(ExtensionContext context) {
         ApplicationContext applicationContext = SpringExtension.getApplicationContext(context);
-        PerformanceMonitorAop monitor = applicationContext.getBean(PerformanceMonitorAop.class);
-        monitor.setFlag();
+        PerformanceMonitorAop aop = applicationContext.getBean(PerformanceMonitorAop.class);
+        aop.setFlag();
+    }
+
+    @Override
+    public void afterEach(ExtensionContext context) {
+        ApplicationContext applicationContext = SpringExtension.getApplicationContext(context);
+        PerformanceMonitor monitor = applicationContext.getBean(PerformanceMonitor.class);
+        log.info(monitor.toString());
     }
 }

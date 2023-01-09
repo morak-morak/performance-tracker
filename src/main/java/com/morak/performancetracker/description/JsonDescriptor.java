0382f24bdbc83f7db4374b8b250262c2c2817b6d
Kim Seunglae <obey1342@gmail.com>
2023-01-09 15:04:19 +0900

    refactor: 생성자 주입방식 변경 및 파일이름 형식 변경

--- src/main/java/com/morak/performancetracker/description/JsonDescriptor.java
+++ src/main/java/com/morak/performancetracker/description/JsonDescriptor.java
@@ -7,7 +7,8 @@ import com.morak.performancetracker.context.Scope;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
@@ -18,24 +19,42 @@ public class JsonDescriptor implements Descriptor {
 
     private static final String JSON_FORMAT = ".json";
 
-    @Value("${com.morak.performance-tracker.logs.path:./logs/}")
-    private String filePath;
+    private final String filePath;
+    private final ObjectMapper objectMapper;
 
-    @Value("${com.morak.performance-tracker.logs.file:performance}")
-    private String fileName;
-
-    @Autowired
-    private ObjectMapper objectMapper;
+    public JsonDescriptor(@Value("${com.morak.performance-tracker.logs.path:./logs/}") String filePath,
+                          ObjectMapper objectMapper) {
+        this.filePath = filePath;
+        this.objectMapper = objectMapper;
+    }
 
     @Override
     public void describe(Context context) {
-        File file = new File(filePath + fileName + JSON_FORMAT);
+        File file = new File(filePath + createFileName(context) + JSON_FORMAT);
         try (FileWriter fileWriter = new FileWriter(file, true);
              SequenceWriter seqWriter = objectMapper.writer().writeValuesAsArray(fileWriter)) {
             writeToFile(context, seqWriter);
         } catch (IOException e) {
             throw new RuntimeException("I/O error writing context");
         }
+    }
+
+    private String createFileName(Context context) {
+        System.out.println(getTargetClassName(context) + getNowDate());
+        return getTargetClassName(context) + getNowDate();
+    }
+
+    private String getTargetClassName(Context context) {
+        String[] filePaths = context.getName()
+                .split("\\.");
+        int length = filePaths.length;
+        return filePaths[length - 1];
+    }
+
+    private String getNowDate() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd-HH:mm:ss");
+        Date date = new Date();
+        return dateFormat.format(date);
     }
 
     private void writeToFile(Context context, SequenceWriter seqWriter) throws IOException {

ac1e7712f213dd133c51d6119bdf55476959ae06
cjlee38 <cjlee38@github.com>
2023-01-13 19:25:24 +0900

    refactor: change scan instead of manual registration

--- src/main/java/com/morak/performancetracker/configuration/PerformanceConfiguration.java
+++ src/main/java/com/morak/performancetracker/configuration/PerformanceConfiguration.java
@@ -1,5 +1,6 @@
 package com.morak.performancetracker.configuration;
 
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +9,6 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @Configuration
 @ComponentScan(basePackages = "com.morak.performancetracker")
 @EnableAspectJAutoProxy(proxyTargetClass = true)
-@EnableConfigurationProperties(value = DescriptorProperties.class)
+@ConfigurationPropertiesScan
 public class PerformanceConfiguration {
 }

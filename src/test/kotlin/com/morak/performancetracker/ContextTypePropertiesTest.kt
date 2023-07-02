c2947070f9898f6b3a39a4aa0bcd34abba231231
cjlee38 <gptpem38@gmail.com>
2023-07-02 16:56:43 +0900

    style: fix typo

--- src/test/kotlin/com/morak/performancetracker/ContextTypePropertiesTest.kt
+++ src/test/kotlin/com/morak/performancetracker/ContextTypePropertiesTest.kt
@@ -14,12 +14,12 @@ import org.springframework.test.context.TestPropertySource
 @PerformanceTracker
 class ContextTypePropertiesTest {
     @Nested
-    inner class Context_Type에_아무것도_없는경우 {
+    inner class `Context Type에 아무것도 없는경우` {
         @Autowired
         private lateinit var managers: List<ContextManager>
 
         @Test
-        fun 모든_Type이_등록된다() {
+        fun `모든 Type이 등록된다`() {
             assertThat(managers).hasOnlyElementsOfTypes(
                 MethodContextManager::class.java,
                 EndpointContextManager::class.java
@@ -29,35 +29,35 @@ class ContextTypePropertiesTest {
 
     @Nested
     @TestPropertySource(properties = ["com.morak.performance-tracker.context.type = method"])
-    inner class Context_Type에_Method만_있는_경우 {
+    inner class `Context Type에 Method만 있는 경우` {
         @Autowired
         private lateinit var managers: List<ContextManager>
 
         @Test
-        fun Method_타입만_등록된다() {
+        fun `Method 타입만 등록된다`() {
             assertThat(managers).hasExactlyElementsOfTypes(MethodContextManager::class.java)
         }
     }
 
     @Nested
     @TestPropertySource(properties = ["com.morak.performance-tracker.context.type = endpoint"])
-    inner class Context_Type에_Endpoint만_있는_경우 {
+    inner class `ContextType에 Endpoint만 있는 경우` {
         @Autowired
         private lateinit var managers: List<ContextManager>
 
         @Test
-        fun Endpoint_타입만_등록된다() {
+        fun `Endpoint 타입만 등록된다`() {
             assertThat(managers).hasExactlyElementsOfTypes(EndpointContextManager::class.java)
         }
     }
 
     @Nested
     @TestPropertySource(properties = ["com.morak.performance-tracker.context.type = method, endpoint"])
-    internal inner class Context_Type에_여러개를_설정하는_경우 {
+    inner class `ContextType에 여러개를 설정하는 경우` {
         @Autowired
         private lateinit var managers: List<ContextManager>
         @Test
-        fun 설정한_모든_타입이_등록된다() {
+        fun `설정한 모든 타입이 등록된다`() {
             println("managers = ${managers}")
             assertThat(managers).hasOnlyElementsOfTypes(
                 MethodContextManager::class.java,

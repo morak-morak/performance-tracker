c52d39cc087665d21e2f162393fac322a2b581db
sungsan <kunga077@gmail.com>
2023-07-03 15:09:18 +0900

    feat: 스트링 확장함수 추가

--- src/main/kotlin/com/morak/performancetracker/utils/KotlinUtils.kt
+++ src/main/kotlin/com/morak/performancetracker/utils/KotlinUtils.kt
@@ -4,3 +4,15 @@ fun <T> List<T>.average(selector: (T) -> Double): Double {
     if (this.isEmpty()) return 0.toDouble()
     return this.sumOf(selector) / this.size
 }
+
+fun String.substringUntil(delimiter: Char): String {
+    val lastIndex = this.lastIndexOf(delimiter)
+    return if (lastIndex == -1) this
+    else this.substring(lastIndex + 1)
+}
+
+fun String.substringFrom(delimiter: Char): String {
+    val lastIndex = this.lastIndexOf(delimiter)
+    return if (lastIndex == -1) this
+    else this.substring(0, lastIndex)
+}
